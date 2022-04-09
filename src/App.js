574938993419d98595216932e9e0ff97baf28e4b
Vladimir Fedarenka <vfedarenka074@gmail.com>
2022-04-09 23:06:02 +0200

    add final functional and change open API for convertin currencies

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import './App.css';
 import Currency from './Currency'
 import {useEffect, useState} from 'react'
 
-const BASE_URL = 'http://api.exchangeratesapi.io/v1/latest?access_key=5d30bf6463063bd9f913b6bd184cf266'
+// const BASE_URL = 'http://api.exchangeratesapi.io/v1/latest?access_key=5d30bf6463063bd9f913b6bd184cf266'
+
+const BASE_URL   = 'https://free.currconv.com/api/v7/currencies?apiKey=1dbfb5b829c15c1df18f'
 
 function App() {
 
@@ -12,7 +14,6 @@ function App() {
   const [rate, setRate] = useState()
   const [amount, setAmount] = useState(1)
   const [inFromAmount, setInFromAmount] = useState(true)
-  console.log(1)
 
   let toAmount, fromAmount
   if(inFromAmount){
@@ -28,20 +29,32 @@ function App() {
     fetch(BASE_URL)
       .then(res=>res.json())
       .then(data=>{
-        const firstCurrency = Object.keys(data.rates)[0]
-        setCurrOptions([data.base, ...Object.keys(data.rates)])
-        setFromCurr(data.base)
-        setToCurr(firstCurrency)
-        setRate(data.rates[firstCurrency])
+        const firstCurrency = Object.keys(data.results)[2]
+        setCurrOptions([ ...Object.keys(data.results)])
+        setFromCurr(firstCurrency)
+        setToCurr(Object.keys(data.results)[2])
+        setRate(data.results[firstCurrency])
+        // console.log(data)
       })
+      
   },[])
+
+  useEffect(()=>{
+    fetch(`https://free.currconv.com/api/v7/convert?q=${fromCurr}_${toCurr}&compact=ultra&apiKey=3f79e0653d720734ff61`)
+      .then(res=>res.json())
+      .then(data=>{
+        setRate(Object.values(data))
+      })
+      
+  },[fromCurr, toCurr])
 
   useEffect(() => {
     if (fromCurr != null && toCurr != null) {
-      fetch(`${BASE_URL}?base=${fromCurr}&symbols=${toCurr}`)
+      fetch(`https://free.currconv.com/api/v7/convert?q=${fromCurr}_${toCurr}&compact=ultra&apiKey=3f79e0653d720734ff61`)
         .then(res => res.json())
-        .then(data => setRate(data.rates[toCurr]))
+        .then(data => setRate(Object.values(data.results)[toCurr]))
     }
+    console.log(`https://free.currconv.com/api/v7/convert?q=${fromCurr}_${toCurr}&compact=ultra&apiKey=3f79e0653d720734ff61`)
   }, [fromCurr, toCurr])
 
   function handleFromAmount(e){
